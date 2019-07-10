65f2582fb7b327dc734d898f5bac98901bd4f141
archification <archification@gmail.com>
2019-07-09 23:37:27 -0500

    first commit

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Enter your name: ");
+    let mut name = String::new();
+    io::stdin().read_line(&mut name)
+        .expect("Failed to read line");
+
+    println!("Guess a five digit number");
+
+    let secret_number = rand::thread_rng().gen_range(10000, 99999);
+    let mut tries = 0;
+
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        //tries = tries + 1;
+        tries += 1;
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("higher"),
+            Ordering::Greater => println!("lower"),
+            Ordering::Equal => {
+                println!("Well done {}, you guessed my number from {} tries!", name.trim(), tries);
+
+                break;
+            }
+        }
+    }
+}
