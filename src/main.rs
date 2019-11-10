fc980ae1698e1d392855d19f72ff02a641c6cada
archification <archification@gmail.com>
2019-11-10 03:02:58 -0600

    cleaning

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,25 @@
 extern crate rand;
 extern crate termion;
 
-use std::io;
+use std::io::{stdin, stdout, Write};
 use std::cmp::Ordering;
 use rand::Rng;
-use termion::clear;
+use termion::{clear};
 
 fn main() {
     println!("{}", clear::All);
+    let _ = stdout().flush();
     println!("Enter your name: ");
     let mut name = String::new();
-    io::stdin().read_line(&mut name)
+    stdin().read_line(&mut name)
         .expect("Failed to read line.");
-
     println!("Guess a five digit number.");
-
     let secret_number = rand::thread_rng().gen_range(10000, 99999);
     let mut tries = 0;
 
     loop {
         let mut guess = String::new();
-        io::stdin().read_line(&mut guess)
+        stdin().read_line(&mut guess)
             .expect("Failed to read line");
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
